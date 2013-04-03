5ad42486b04651c1d31f0c6792c36b70908b007e
Vladimir Livin <vladimir@livesystems.ru>
2013-04-03 23:48:47 +0400

    Added word list creation, and words selection.

--- AttributedString/AttributedViewController.m
+++ AttributedString/AttributedViewController.m
@@ -9,21 +9,49 @@
 #import "AttributedViewController.h"
 
 @interface AttributedViewController ()
+@property (strong, nonatomic) NSArray* wordList;
+@property (weak, nonatomic) IBOutlet UILabel *label;
+@property (weak, nonatomic) IBOutlet UIStepper *selectedWorkStepper;
+@property (weak, nonatomic) IBOutlet UILabel *selectedWordLabel;
 
 @end
 
 @implementation AttributedViewController
 
+- (NSArray*) wordList
+{
+	if (!_wordList) {
+		_wordList = [self pickWordListFromLabel];
+	}
+	return _wordList;
+}
+
+- (NSArray*) pickWordListFromLabel
+{
+	NSArray* list = [[self.label.attributedText string] componentsSeparatedByCharactersInSet: [NSCharacterSet whitespaceAndNewlineCharacterSet]];
+	
+	return [list count]?list:@[@""];
+}
+
+- (void) updateSelectedWorkStepper
+{
+	self.selectedWorkStepper.maximumValue = [self.wordList count] - 1;
+}
+
+- (void) updateSelectedWordLabel
+{
+	self.selectedWordLabel.text = self.wordList[(NSUInteger)self.selectedWorkStepper.value];
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	[self updateSelectedWorkStepper];
+	[self updateSelectedWordLabel];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)selectedNewWord {
+	[self updateSelectedWordLabel];
 }
 
 @end
